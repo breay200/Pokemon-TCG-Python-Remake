fe510c76e23c31ce6484113eba8102adfb38d6ad
argonboron <argonboron@hotmail.co.uk>
2021-10-02 16:11:43 +0100

    use self and switch case

--- classes/Menu.py
+++ classes/Menu.py
@@ -10,13 +10,13 @@ class Menu():
         print("Welcome to Pokemon Trading Card Game - Python Edition!")
         login_form = LoginForm()
         username = login_form.check_account()
-        menu = Menu()
-        menu.game_menu(username)
+        self.game_menu(username)
         #print(username)
 
     def game_menu(self, username):
         print("Game Menu")
-        while True:
+        shouldContinue = True
+        while shouldContinue:
             print(f"Welcome {username}! Please select an option from the menu.")
             print("1. Start New Match")
             print("2. Change Deck")
@@ -27,29 +27,30 @@ class Menu():
                 response = int(response)
             except ValueError:
                 print("You did not enter a valid number!")
-            if response == 1:
-                #start match method
-                print(1)
-                break
-            elif response == 2:
-                #change deck method
-                deck = Deck()
-                decks = deck.get_decks("set1.json")
-                deck.set_active_deck(decks)
-                print(deck)
-                deck.load_deck("set1.json")
-                break
-            elif response == 3:
-                user = User(username)
-                if check_in_file("player_data.txt", username):
-                    user.load_user_data()
-                    print(user)
-                else:
-                    user.create_user_data()
-                    print(user)
-            elif response == 4:
-                print("Quitting Program")
-                quit()
-            else:
-                pass
+            match response:
+                case 1:
+                    #start match method
+                    print(1)
+                    break
+                case 2:
+                    #change deck method
+                    deck = Deck()
+                    decks = deck.get_decks("set1.json")
+                    deck.set_active_deck(decks)
+                    print(deck)
+                    deck.load_deck("set1.json")
+                    break
+                case 3:
+                    user = User(username)
+                    if check_in_file("player_data.txt", username):
+                        user.load_user_data()
+                        print(user)
+                    else:
+                        user.create_user_data()
+                        print(user)
+                case 4:
+                    print("Quitting Program")
+                    shouldContinue = False
+                case _:
+                    pass
                 
