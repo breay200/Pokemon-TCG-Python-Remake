5753fe1ad5100b88ada1f0e282acceb251ca6448
reayboi <brandonreay2000@gmail.com>
2021-10-17 13:22:44 +0100

    nice

--- classes/Game.py
+++ classes/Game.py
@@ -15,9 +15,22 @@ from classes.config import *
 class Game():
     def __init__(self, user):
         self.user = user
+
         self.game_frame = tk.Frame(Config.master)
+
+        self.welcome_label = tk.Label(self.game_frame, text=f"HEY {user.username}\nWELCOME TO POKEMON TCG - PYTHON EDITION")
+        self.welcome_label.grid(column=0, row=0, columnspan=5)
+
+        self.start_btn = tk.Button(self.game_frame, text="Start New Match")
+        self.start_btn.grid(column=0,row=1)
+
+        self.settings_btn = tk.Button(self.game_frame, text="Settings")
+        self.settings_btn.grid(column=0, row=2)
+
+        self.exit_btn = tk.Button(self.game_frame, text="Quit Game")
+        self.exit_btn.grid(column=0, row=3)
+
         self.game_frame.grid(column=0, row=0)
-        print(user)
 
     def main_game_loop(self):
         deck = Deck()
