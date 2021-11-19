24ce3bac3aa9c47afb3f914624046cf0dfbd8095
reayboi <brandonreay2000@gmail.com>
2021-11-19 15:31:25 +0000

    update - created networking class and functional lobby with object oriented design

--- classes/Lobby.py
+++ classes/Lobby.py
@@ -1,53 +1,58 @@
-import tkinter as tk
-from tkinter.constants import DISABLED, NORMAL
+from tkinter.constants import DISABLED, NE, NORMAL
 from classes.config import Config
-import socket
-from threading import Thread
+from classes.Networking import *
+import tkinter as tk
 
 class Lobby():
     def __init__(self):
+        self.connectivity = Networking()
+
         self.lobby_frame = tk.Frame(Config.master)
-        self.lobby_title = tk.Label(self.lobby_frame, text="LOBBY")
+        
+        self.lobby_title = tk.Label(self.lobby_frame, text="Lobby")
         self.lobby_title.grid(column=0, row=0, sticky="ew")
-        self.connection_status = "not connected"
-        self.connection_status_label = tk.Label(self.lobby_frame, text=f"status: {self.connection_status}")
+        
+        self.connection_status_label = tk.Label(self.lobby_frame, text=f"connection status: {self.connectivity.status}")
         self.connection_status_label.grid(column=0, row=1)
+
+        self.address = tk.StringVar()
+        self.address_label = tk.Label(self.lobby_frame, text="Enter server IP address: ")
+        self.address_label.grid(column=0, row=2)
+        self.address_entry = tk.Entry(self.lobby_frame, textvariable=self.address)
+        self.address_entry.grid(column=1, row=2)
+
+        self.port = tk.IntVar()
+        self.port_label = tk.Label(self.lobby_frame, text="Enter server port number: ")
+        self.port_label.grid(column=2,row=2)
+        self.port_entry = tk.Entry(self.lobby_frame, textvariable=self.port)
+        self.port_entry.grid(column=3, row=2)
+        
         self.lobby_count = 0
-        self.players_in_lobby_label = tk.Label(self.lobby_frame, text=f"players in lobby: {self.lobby_count}")
-        self.players_in_lobby_label.grid(column=0, row=2)
-        #self.lobby_count_label = tk.Label(self.lobby_frame, text=str(self.lobby_count))
-        #self.lobby_count_label.grid(column=1, row=2)
-        self.player_names = []
+        self.lobby_count_label = tk.Label(self.lobby_frame, text=f"number of available players: {self.lobby_count}")
+        #self.players_in_lobby_label.grid(column=0, row=2)
+
         self.player_frame = tk.Label(self.lobby_frame)
         self.player_frame.grid(column=0, row=3, sticky="ew")
-        self.server_address = ('127.0.0.1', 65432)
-        self.server_address_label = tk.Label(self.lobby_frame, text=f"server details: {self.server_address}")
-        self.server_address_label.grid(column=0 ,row=4, columnspan=3)
+
+        #self.server_address = ('127.0.0.1', 65432)
+        #self.server_address_label = tk.Label(self.lobby_frame, text=f"server details: {self.server_address}")
+        #self.server_address_label.grid(column=0 ,row=4, columnspan=3)
+
         self.connect_btn = tk.Button(self.lobby_frame, text="Connect to Server", command=self.connect_to_server)
         self.connect_btn.grid(column=0, row=5, columnspan=3)
+
         self.lobby_frame.grid(column=0, row=0)
-        self.data = []
     
-    def create_thread(self):
-        self.lobby_thread = Thread(target=self.connect_to_server, daemon=True)
-        self.connect_btn.configure(text="lolololo")
-        self.connect_btn.grid(column=0, row=5, columnspan=3)
-        self.lobby_frame.grid(column=0, row=0)
-
-
     def connect_to_server(self):
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        print('connecting to %s port %s' % self.server_address)
+        address = self.address.get()
+        port = self.port.get()
         try:
-            sock.connect(self.server_address)
+            address = str(address)
+            port = int(port)
         except Exception as e:
-            print(e)
-        while True:
-            try:
-                message = sock.recv(1024).decode()
-                if len(message)>1:
-                    self.data.append(message)
-                    for x in self.data:
-                        print(x)
-            except Exception as e:
-                print(e)   
+            print("error converting types in connect_to_server method in lobby class", e)
+            return
+        server_address = (address, port)
+        self.connectivity.set_server_address(server_address)
+        self.connectivity.connect_to_server()
+
---
+++ classes/Networking.py
@@ -0,0 +1,53 @@
+from os import error
+from threading import Thread
+import socket
+
+class Networking():
+    def __init__(self) -> None:
+        self.header_length = 1024
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.status = "not connected"
+        self.write_file = "received_data.txt"
+        self.server_address = None
+    
+    def set_server_address(self, server_address):
+        """set_server_address: sets the server address"""
+        self.server_address = server_address
+
+    def connect_to_server(self):
+        """connect_to_server: connects the socket to the server, then creates and starts the listening thread."""
+        print('connecting to %s port %s' % self.server_address)
+        self.sock.connect(self.server_address)
+        #self.listen_thread = Thread(target = self.listen, args=(self.sock,), daemon=True)
+        self.listen_thread = Thread(target = self.listen, daemon=True)
+        self.listen_thread.start()
+
+    def listen(self):
+        """receive: receives data from the server and writes it to file."""
+        while True:
+            try:
+                data_header = self.sock.recv(self.header_length)
+                data_length = int(float(data_header.decode('utf-8').strip()))
+                data = self.sock.recv(data_length).decode('utf-8')
+                
+                if data == 'user 1 received':
+                    #status["sending"] = False
+                    pass
+                elif 'user2' in data:
+                    print("received data from user 2", data)
+                    with open("received_data.txt", "a") as file1:
+                        file1.write(data + "\n")
+                        #no_data = False
+                elif 'user1' in data:
+                    #print("this is my sent data")
+                    pass
+            except Exception as e:
+                print(e)
+    
+    def send(self, data):
+        """send: sends data to the server with data supplied by the user."""
+        try:
+            data_header = f"{len(data):<{self.header_length}}".encode('utf-8')
+            assert self.sock.send(data_header+data.encode('utf-8'))
+        except Exception as e:
+            print(f"Exception in send method of networking class: {e}")
