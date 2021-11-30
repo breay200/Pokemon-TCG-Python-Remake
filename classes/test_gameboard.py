fa574d2e327b0782de3c75f33b05ec5728ffed96
reayboi <brandonreay2000@gmail.com>
2021-11-30 13:58:09 +0000

    fixed card scaling resize for prize

--- classes/test_gameboard.py
+++ classes/test_gameboard.py
@@ -23,20 +23,20 @@ def add_to_bench():
 
 def load_prize():
     for x in range(6):
-        prize_widget_list.append(tk.Button(prize, image=deck_img, width=width*0.08, height=height*0.2, borderwidth=0, highlightthickness=0))
+        prize_widget_list.append(tk.Button(prize, image=prize_img, width=prize_img_width, height=prize_img_height, borderwidth=0, highlightthickness=0))
     
     for x in prize_widget_list:
-        if prize_widget_list.index(x) in [0, 2, 4]:
+        if prize_widget_list.index(x) in [0,2,4]:
             x_coordinates = 0
         elif prize_widget_list.index(x) in [1,3,5]:
-            x_coordinates  = (width*0.08)
+            x_coordinates  = prize_img_width
         
-        if prize_widget_list.index(x) in [0, 1]:
+        if prize_widget_list.index(x) in [0,1]:
             y_coordinates = 0
-        elif prize_widget_list.index(x) in [2, 3]:
-            y_coordinates = height*0.2
-        elif prize_widget_list.index(x) in [4, 5]:
-            y_coordinates = height*0.4
+        elif prize_widget_list.index(x) in [2,3]:
+            y_coordinates = prize_img_height
+        elif prize_widget_list.index(x) in [4,5]:
+            y_coordinates = prize_img_height*2
         
         x.place(x=x_coordinates, y=y_coordinates)
             
@@ -58,28 +58,22 @@ discard_img = Image.open("images/card_space.png").resize((int(width*0.08), int(h
 discard_img = ImageTk.PhotoImage(discard_img)
 
 player = tk.Frame(master, width=width, height=height/2, bg="blue")
-prize = tk.Frame(player, width=width*0.16, height=height*0.45, highlightthickness=5, highlightbackground="black")
+prize = tk.Frame(player, width=width*0.15, height=height*0.5)
+#print(width*0.16, height*0.45)
 bench = tk.Frame(player, width=width*0.5, height=height*0.25, highlightthickness=5, highlightbackground="black")
 discard = tk.Button(player, image=discard_img, width=width*0.08, height=height*0.2, bg="blue", borderwidth=0, highlightthickness=0)
 deck = tk.Button(player, image=deck_img, width=width*0.08, height=height*0.2, bg="blue", borderwidth=0, highlightthickness=0, command=add_to_bench)
 active = tk.Frame(player, width=width*0.1, height=height*0.2, bg="white")
 
+prize_img_width = int(width*0.075)
+prize_img_height = int(height*0.16666)
+prize_img = Image.open("images/pokemon_back.png").resize((prize_img_width, prize_img_height))
+prize_img = ImageTk.PhotoImage(prize_img)
+
 btn_widget_list = []
 prize_widget_list = []
 
 load_prize()
-"""
-for x in range(5):
-    btn_widget_list.append(tk.Button(bench, image=deck_img, width=width*0.08, height=height*0.2, borderwidth=0, highlightthickness=0))
-
-x_coordinates = 0
-for x in btn_widget_list:
-    if btn_widget_list.index(x) == 0:
-        pass
-    else:
-        x_coordinates += (width*0.015)+(width*0.08)
-    x.place(x=x_coordinates, y=0)
-"""
 
 prize.place(x=width*0.02, y=0)
 bench.place(x=(width*0.02+width*0.2+width*0.03), y=height*0.225)
