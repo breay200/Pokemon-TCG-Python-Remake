8b695fe87c2ac5991cd94253f39c0b1c852486f6
reayboi <brandonreay2000@gmail.com>
2021-12-02 18:22:43 +0000

    cosmetic changes to main menu

--- classes/MainMenu.py
+++ classes/MainMenu.py
@@ -8,19 +8,19 @@ class MainMenu:
         print(width, height)
 
 
-        self.main_frame = tk.Frame(Config.master, width=width, height=height, bg="red")
+        self.main_frame = tk.Frame(Config.master, width=width, height=height)
 
-        self.title_img = Image.open("images/menu_logo.png").resize((int(width), int(height)))
+        self.title_img = Image.open("images/menu_logo.png").resize((int(width*0.8), int(height*0.8)))
         self.title_img = ImageTk.PhotoImage(self.title_img)
 
-        self.img_label = tk.Label(self.main_frame, image=self.title_img, borderwidth=0, highlightthickness=0, width=width, height=height)
+        self.img_label = tk.Label(self.main_frame, image=self.title_img, borderwidth=0, highlightthickness=0, width=int(width), height=int(height))
         self.img_label.place(x=0, y=0)
 
-        self.start_button = tk.Button(self.main_frame, text="Start", command=self.start, width=int(width/100), height=int(height/15), bg="blue")
-        self.start_button.place(x=(width*0.5), y=(200))
+        self.start_button = tk.Button(self.main_frame, text="Start", command=self.start, width=int(width*0.015), height=int(height*0.005), bg="blue")
+        self.start_button.place(x=((width*0.5)-(width*0.015)), y=(height*0.6))
         
-        self.quit_button = tk.Button(self.main_frame, text="Quit", command=self.quit, width=int(width/100), height=int(height/100), bg="green")
-        self.quit_button.place(x=(width*0.5), y=(height*0.3))
+        self.quit_button = tk.Button(self.main_frame, text="Quit", command=self.quit, width=int(width*0.015), height=int(height*0.005), bg="green")
+        self.quit_button.place(x=((width*0.5)-(width*0.015)), y=(height*0.7))
 
         self.main_frame.place(x=0,y=0)
 
