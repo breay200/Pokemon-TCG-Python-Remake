a09d482cc032d8c635e8095bf61f30a5a7960f46
reayboi <brandonreay2000@gmail.com>
2021-12-15 14:43:16 +0000

    fixed window issues with login page

--- classes/LoginForm.py
+++ classes/LoginForm.py
@@ -53,7 +53,7 @@ class LoginForm(Form):
 
         self.login_forgot_btn = tk.Button(self.login_frame, text="Forgot Password", command=self.forgot_password)
 
-        self.login_error_label = tk.Label(self.login_frame)
+        self.login_error_label = tk.Label(self.login_frame, bg="white", fg="red")
 
         self.login_create_btn = tk.Button(self.login_frame, text="Create Account", command=self.create_account)
 
@@ -83,14 +83,14 @@ class LoginForm(Form):
                     self.login_frame.destroy()
                     mainmenu = MainMenu(user)
                 else:
-                    self.login_error_label.configure(text="You did not login successfully.")
-                    self.login_error_label.grid(column=3, row=5)
+                    self.login_error_label.configure(text="There was an error with the username or password.")
+                    self.login_error_label.place(x=0, y=self.side_bar_height*0.7)
             else:
-                self.login_error_label.configure(text="User does not exist")
-                self.login_error_label.grid(column=3, row=5)
+                self.login_error_label.configure(text="This username does not exist")
+                self.login_error_label.place(x=0, y=self.side_bar_height*0.7)
         elif not username:
             self.login_error_label.configure(text="You did not enter a username")
-            self.login_error_label.grid(column=3, row=5)
+            self.login_error_label.place(x=0, y=self.side_bar_height*0.7)
 
     def create_account(self):
         self.login_frame.destroy()
@@ -102,27 +102,12 @@ class LoginForm(Form):
                 password_hash = hashlib.md5(str(password).encode('utf-8'))
                 password = password_hash.hexdigest()
                 if check_password(username, password):
-                    self.login_error_label.configure(text="You have logged in successfully!")
-                    self.login_error_label.grid(column=3, row=6)
-                    #return a True value if login success
                     return True
                 else:
-                    self.failed_logins += 1
-                    self.login_error_label.configure(text="You have failed to login.")
-                    self.login_error_label.grid(column=3, row=6)
-                    self.login_fail_label.configure(text=f"There are {3-self.failed_logins} attempts remaining")
-                    self.login_fail_label.grid(column=4, row=6)
-                    if self.failed_logins >= 3:
-                        print(self.failed_logins)
-                        self.login_error_label.configure("You have failed too many times. Account has been locked.")
-                        self.login_error_label.grid(column=3, row=6)
-                        self.login_submit_btn.grid_remove()
-                        #need to create account lock method
-                        #return a False value if login failed
-                        return False
+                    return False
             else:
                 self.login_error_label.configure(text="You need to enter a username and password")
-                self.login_error_label.grid(column=3, row=5)
+                self.login_error_label.place(x=0, y=self.side_bar_height*0.7)
 
     def forgot_password(self):
         """forgot_password: call this method when the user clicks on the forgot password button. Checks to see if the user has entered a username, and continues with the password reset."""
