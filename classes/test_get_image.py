76c62dcf397000bbc21b85eccf3797c2e1d99283
reayboi <brandonreay2000@gmail.com>
2022-01-03 20:00:38 +0000

    push

--- classes/test_get_image.py
+++ classes/test_get_image.py
@@ -10,17 +10,19 @@ master.title("TCG REMAKE")
 canvas = tk.Canvas(master, width=width, height=height)
 canvas.place(x=0,y=0)
 
-rec = canvas.create_rectangle(0, 0, 30, 30, fill="red")
+rec = canvas.create_rectangle(0, 0, 100, 100, fill="red")
 def test(event=None, x=0):
-    x += 12
+    x = 12
     canvas.move(rec, x, 0)
-    if x == 400:
+    x1, y1, x2, x2 = canvas.coords(rec)
+    if x1 >= 400:
         print("done")
+        master.unbind("<Return>")
     else:
         canvas.after(16, test)
 
 x = 0
-master.bind("<Return>", lambda event: test(event, x))
+master.bind("<Return>", lambda event: test(event))
 # canvas.after(1000, lambda: canvas.move(rec, 0, 400))
 # #canvas.move(rec, 400, 400)
 master.mainloop()
