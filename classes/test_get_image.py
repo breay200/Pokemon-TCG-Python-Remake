3831d1e8bb31afc6ed786c64fbb0f33775ee62f6
reayboi <brandonreay2000@gmail.com>
2022-01-03 21:10:10 +0000

    created first polygon

--- classes/test_get_image.py
+++ classes/test_get_image.py
@@ -10,18 +10,24 @@ master.title("TCG REMAKE")
 canvas = tk.Canvas(master, width=width, height=height)
 canvas.place(x=0,y=0)
 
-rec = canvas.create_rectangle(0, 0, 100, 100, fill="red")
-def test(event=None, x=0):
-    x = 12
-    canvas.move(rec, x, 0)
-    x1, y1, x2, x2 = canvas.coords(rec)
-    if x1 >= 400:
-        print("done")
-        master.unbind("<Return>")
-    else:
-        canvas.after(16, test)
+# button = tk.Button(canvas, width=100, height=100, bg="red")
+# button.place(x=0, y=0)
+points = [0, 0, 100, 0, 100, 100, 70, 100, 70, 30, 0, 30]
+canvas.create_polygon(points, fill="red")
 
-x = 0
+# rec = canvas.create_rectangle(0, 0, 100, 100, fill="red")
+
+# def test(event=None, x=0):
+#     x = 12
+#     canvas.move(rec, x, 0)
+#     x1, y1, x2, x2 = canvas.coords(rec)
+#     if x1 >= 400:
+#         print("done")
+#         master.unbind("<Return>")
+#     else:
+#         canvas.after(16, test)
+
+# x = 0
 master.bind("<Return>", lambda event: test(event))
 # canvas.after(1000, lambda: canvas.move(rec, 0, 400))
 # #canvas.move(rec, 400, 400)
